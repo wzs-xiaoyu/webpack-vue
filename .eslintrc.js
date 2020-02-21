5aa76e598a8ba6d9d5c5d1c1ac64790c6bc086a0
wangzs <my_xiaoyufamily@163.com>
2020-02-21 11:27:31 +0800

    refactor: eslint+prettier配置

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: ["eslint:recommended", "plugin:vue/essential", "prettier"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module"
+	},
+	plugins: ["vue"],
+	rules: {
+		"no-console": 0, //禁止使用console
+		eqeqeq: 0, //必须使用全等
+		"no-debugger": 1, //禁止使用debugger
+		"no-unused-vars": [1, { vars: "all", args: "after-used" }] //不能有声明后未被使用的变量或参数
+	}
+};
